6e91acda8ddf9021379e1aba9683c1c8c207d61e
edison <417524607@qq.com>
2019-06-13 15:56:20 +0800

    update code

--- src/Test.java
+++ src/Test.java
@@ -1,2 +1,13 @@
-public class Test {
-}
+const val DOMAIN = "https://qualityassurance-amh-gbb-sit2.p2g.netd2.hsbc.com.hk"
+
+
+buildConfigField("String", "MAUTH_SERVER_ADDRESS", "\"https://qualityassurance-amh-dbbsp-mauth-dev.p2g.netd2.hsbc.com.hk\"")
+            buildConfigField("String", "MAUTH_APPLICATION_ID", "\"hsbcbizexpress\"")
+            buildConfigField("String", "MAUTH_TOKEN_NAME", "\"hsbcbizexpress\"")
+            buildConfigField("String", "MAUTH_TOKEN_API", "\"1399a590-b894-48e2-b164-ffab82e23eae\"")
+
+
+
+
+<string name="entity_url">https://netmobile:MobP8w@www.hkg1vl0077.p2g.netd2.hsbc.com.hk/mobile/GBB/2019R4/SIT/entitylist-GBB.xml</string>
+    <string name="checksum_url">https://netmobile:MobP8w@www.hkg1vl0077.p2g.netd2.hsbc.com.hk/mobile/GBB/2019R4/SIT/checksum-GBB.xml</string>
